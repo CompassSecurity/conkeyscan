2191aada035ed3517413c610a80955d27123a754
frjcomp <frj1@securelogon.ch>
2024-03-05 15:48:42 +0100

    retry packaging

--- conkeyscan/conkeyscan.py
+++ conkeyscan/conkeyscan.py
@@ -133,7 +133,7 @@ def main(
     url: "u",
     username: "usr",
     password: "pwd",
-    dict_path: "d" = pkg_resources.resource_filename("conkeyscan", "static/dict.txt"),
+    dict_path: "d" = pkg_resources.resource_filename("conkeyscan", "config/dict.txt"),
     disable_ssl_checks: "k" = True,
     rate_limit: "r" = 100,
     proxy: "p" = "",
--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="conkeyscan",
+    author="Jan Friedli",
+    description="A Pentesters Confluence Keyword Scanner",
     version="{{VERSION_PLACEHOLDER}}",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -16,7 +18,10 @@ setup(
         "readchar",
         "PySocks>=1",
     ],
-    entry_points={"console_scripts": ["conkeyscan = conkeyscan:entry_point"]},
-    packages=find_packages() + ["config"],
+    entry_points={
+        "console_scripts": ["conkeyscan = conkeyscan.conkeyscan:entry_point"]
+    },
+    packages=find_packages(),
     include_package_data=True,
+    package_data={"conkeyscan.config": ["*.txt"]},
 )
